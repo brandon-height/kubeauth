331302f866d85cbedf41f903b87677d5f63e55de
Brandon Height <brandon@driver.xyz>
2018-07-20 11:48:10 -0700

    Added a few tests. Need more though

--- providers/gcloud_test.go
+++ providers/gcloud_test.go
@@ -1,1 +1,24 @@
 package providers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TODO: overwrite the exec.Command stuff to we aren't actually executing
+// 	     shell commands. Need mocks.
+
+func Test_ReadAccounts(t *testing.T) {
+	gcloud := GCloudProvider{}
+	accs, err := gcloud.ReadAccounts()
+	assert.NotNil(t, accs)
+	assert.NoError(t, err)
+}
+
+func Test_SelectAccount(t *testing.T) {
+	gcloud := GCloudProvider{}
+	err := gcloud.SelectAccount("")
+	// Even an empty string would come back exit status 0.
+	assert.NoError(t, err)
+}
